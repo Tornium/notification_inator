c64b45b7584f0f72e177331c366bdd8075373593
tiksan <webmaster@deek.sh>
2026-07-11 14:24:13 -0500

    Added function for generating and parsing the message (#1)
    Signed-off-by: tiksan <webmaster@deek.sh>

--- lib/notification_inator/case_template.ex
+++ lib/notification_inator/case_template.ex
@@ -16,4 +16,19 @@ defmodule Tornium.NotificationInator.CaseTemplate do
 
     %{vm: lua_vm}
   end
+
+  using do
+    quote do
+      def generate_message(render_table) do
+        render_state = Tornium.Utils.tuples_to_map(render_table)
+        notification = ExUnit.configuration()[:notification]
+
+        notification.message_template
+        |> Solid.render!(render_state)
+        |> Kernel.to_string()
+        |> String.replace(["\n", "\t"], "")
+        |> JSON.decode!()
+      end
+    end
+  end
 end
